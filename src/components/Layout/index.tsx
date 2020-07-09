9444287e5e1b2c63472345dc7face966fffb6d62
FrostPriest <johnericjahn@gmail.com>
2020-07-09 11:15:26 -0300

    MenuBar Topside

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 
 import Main from '../Main';
+import MenuBar from '../MenuBar';
 import { Container, Wrapper } from './styles';
 
 const layout: React.FC = () => {
   return (
     <Container>
       <Wrapper>
-        {/* <MenuBar /> */}
+        <MenuBar />
         <Main />
         {/* <SideBar /> */}
       </Wrapper>
